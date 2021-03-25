a1a230b8a9d8afa5d2c9d0e3f38df1c5c96cf510
Jake Barber <jabot999@gmail.com>
2021-03-25 13:41:35 -0400

    Token Contract imported

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+const BBSwap = artifacts.require("BBSwap");
+
+module.exports = function(deployer) {
+  deployer.deploy(BBSwap);
+};
---
+++ src/contracts/BBSwap.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: LICENSE
+pragma solidity >=0.5.0;
+
+contract BBSwap {
+    string public name = "BBSwap Instant Exchange";
+
+}
