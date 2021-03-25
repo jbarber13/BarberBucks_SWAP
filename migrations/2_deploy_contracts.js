8560a53215c34407397cba48a13b330ad38e080c
Jake Barber <jabot999@gmail.com>
2021-03-25 14:18:02 -0400

    final revision to deploy contracts, changed name of contract to BBFSwap

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,21 +1,21 @@
 const Token = artifacts.require("Token");
 
-const BBSwap = artifacts.require("BBSwap");
+const BBFSwap = artifacts.require("BBFSwap");
 
 //Need function to be async in order to use await, This insures we are waiting for the actual response for the blockchain instead of simply using the promise
 module.exports = async function(deployer) {
   await deployer.deploy(Token);
   const token = await Token.deployed()
 
-  await deployer.deploy(BBSwap);
-  const bbswap = await BBSwap.deployed()
+  await deployer.deploy(BBFSwap);
+  const bbfswap = await BBFSwap.deployed()
 
   //Transfer all tokens to EthSwap(1 Million * 10^18 (18 decimal places) or totalSupply = 1000000 * (10 ** decimals))
   const totalSupply = '1000000000000000000000000'  
-  await token.transfer(bbswap.address, totalSupply)
+  await token.transfer(bbfswap.address, totalSupply)
 
   /**
-   * Check balance in truffle console with <balance = await token.balanceOf(bbswap.address)><balance.toString()> 
-   * Copy the commands from above into the console first, i.e: <token = await Token.deployed()> <bbswap = await BBSwap.deployed()>
+   * Check balance in truffle console with <balance = await token.balanceOf(bbfswap.address)><balance.toString()> 
+   * Copy the commands from above into the console first, i.e: <token = await Token.deployed()> <bbfswap = await BBFSwap.deployed()>
    */
 };
--- src/contracts/BBSwap.sol
+++ src/contracts/BBSwap.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: LICENSE
 pragma solidity >=0.5.0;
 
-contract BBSwap {
-    string public name = "BBSwap Instant Exchange";
+contract BBFSwap {
+    string public name = "BBFSwap Instant Exchange";
 
 }
